fd5cb93b3110b7f23df0c25890bb8084f1cc7708
Anton <56966986+benbi73tt@users.noreply.github.com>
2022-02-25 18:42:34 +0400

    Initial commit

---
+++ src/main/java/ru/simbirsoft/projecttaskboard/ProjectTaskBoardApplication.java
@@ -0,0 +1,13 @@
+package ru.simbirsoft.projecttaskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaskBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTaskBoardApplication.class, args);
+    }
+
+}
